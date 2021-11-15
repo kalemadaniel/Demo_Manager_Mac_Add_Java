c3058f290e01b2dc25b72a4690739d617059074e
kalemadaniel <kalemadaniel2014@gmail.com>
2021-11-15 15:27:15 +0100

    first commit

---
+++ src/demo_manager_mac_add_java/Demo_Manager_Mac_Add_Java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo_manager_mac_add_java;
+
+/**
+ *
+ * @author Ulb-Kalema
+ */
+public class Demo_Manager_Mac_Add_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
